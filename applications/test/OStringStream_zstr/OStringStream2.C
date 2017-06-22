847bcfff19e76dce80ce0946e32b042a2b65bcea
mattijs <mattijs>
2017-06-22 19:05:03 +0100

    ENH: OStringStream: added compression

--- applications/test/OStringStream_zstr/OStringStream2.C
+++ applications/test/OStringStream_zstr/OStringStream2.C
@@ -21,74 +21,18 @@ License
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
-Application
-    Test-parallel-communicators
-
-Description
-    Checks communication using user-defined communicators
-
 \*---------------------------------------------------------------------------*/
 
-#include "argList.H"
-#include "Time.H"
-#include "zstr.hpp"
-#include "OFstream.H"
 #include "OStringStream2.H"
-
-using namespace Foam;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-int main(int argc, char *argv[])
+void Foam::OStringStream2::print(Ostream& os) const
 {
-    #include "setRootCase.H"
-    //#include "createTime.H"
+    os  << "OStringStream2 " << name() << " : "
+        << "buffer = \n" << str() << Foam::endl;
 
-    std::stringbuf* bufPtr = new std::stringbuf;
-
-    // Stream data into buffer
-    {
-        zstr::ostream os(bufPtr);
-        //ostream os(bufPtr);
-        os << "BLABLA" << std::endl;
-        cout<< "os:" << bufPtr->str() << std::endl;
-    }
-
-    //// Write buffer into OFstream
-    //{
-    //    OFstream os("file_working.gz", IOstream::streamFormat::BINARY);
-    //    os.stdStream() << bufPtr->str();
-    //}
-    {
-        OFstream os("file.gz", IOstream::streamFormat::BINARY);
-        os.writeQuoted(bufPtr->str(), false);
-    }
-
-
-    // Retrieve data from buffer
-    {
-        zstr::istream is(bufPtr);
-        std::string a;
-        is >> a;
-        cout<< "a:" << a << std::endl;
-    }
-
-    // Wrapped up in OStringStream
-    {
-        OStringStream2 os
-        (
-            IOstream::streamFormat::BINARY,
-            IOstream::currentVersion,
-            IOstream::compressionType::COMPRESSED
-        );
-        os << "MORE TESTING" << endl;
-        DebugVar(os.str());
-    }
-
-
-    Pout<< "End\n" << endl;
-
-    return 0;
+    OSstream::print(os);
 }
 
 
