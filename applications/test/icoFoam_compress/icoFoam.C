fbe0f367421c40ecd9b3876b4dfd4c45e137d1b5
mattijs <mattijs>
2018-01-05 08:21:35 +0000

    ENH: zfp: icoFoam start

---
+++ applications/test/icoFoam_compress/icoFoam.C
@@ -0,0 +1,165 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    icoFoam
+
+Description
+    Transient solver for incompressible, laminar flow of Newtonian fluids.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "pisoControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    pisoControl piso(mesh);
+
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    scalarField pData(10*p.size());
+    label pDatai = 0;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "CourantNo.H"
+
+        // Momentum predictor
+
+        fvVectorMatrix UEqn
+        (
+            fvm::ddt(U)
+          + fvm::div(phi, U)
+          - fvm::laplacian(nu, U)
+        );
+
+        if (piso.momentumPredictor())
+        {
+            solve(UEqn == -fvc::grad(p));
+        }
+
+        // --- PISO loop
+        while (piso.correct())
+        {
+            volScalarField rAU(1.0/UEqn.A());
+            volVectorField HbyA(constrainHbyA(rAU*UEqn.H(), U, p));
+            surfaceScalarField phiHbyA
+            (
+                "phiHbyA",
+                fvc::flux(HbyA)
+              + fvc::interpolate(rAU)*fvc::ddtCorr(U, phi)
+            );
+
+            adjustPhi(phiHbyA, U, p);
+
+            // Update the pressure BCs to ensure flux consistency
+            constrainPressure(p, U, phiHbyA, rAU);
+
+            // Non-orthogonal pressure corrector loop
+            while (piso.correctNonOrthogonal())
+            {
+                // Pressure corrector
+
+                fvScalarMatrix pEqn
+                (
+                    fvm::laplacian(rAU, p) == fvc::div(phiHbyA)
+                );
+
+                pEqn.setReference(pRefCell, pRefValue);
+
+                pEqn.solve(mesh.solver(p.select(piso.finalInnerIter())));
+
+                if (piso.finalNonOrthogonalIter())
+                {
+                    phi = phiHbyA - pEqn.flux();
+                }
+            }
+
+            #include "continuityErrs.H"
+
+            U = HbyA - rAU*fvc::grad(p);
+            U.correctBoundaryConditions();
+        }
+
+        runTime.write();
+
+        if (runTime.outputTime())
+        {
+            if (pDatai == 10)
+            {
+                zfp_type type = zfp_type_double;
+                size_t typesize = zfp_type_size(type);
+
+                zfp_field* field = zfp_field_alloc();
+                fp_field_set_pointer(field, pData.begin());
+                zfp_field_set_type(field, zfp_type_double);
+                zfp_field_set_size_2d(field, 10, p.size());
+
+                zfp_stream* zfp = zfp_stream_open(nullptr);
+                bitstream* stream = stream_open(buffer, bufsize);
+                zfp_stream_set_bit_stream(zfp, stream);
+                zfp_stream_set_accuracy(zfp, tolerance);
+    bufsize = zfp_stream_maximum_size(zfp, field);
+
+
+                pDatai = 0;
+            }
+            else
+            {
+                // Shift up
+                for (label i = 0; i < pDatai-1; i++)
+                {
+                    SubList<scalar>(pData, p.size(), pDatai*p.size()) =
+                        SubList<scalar>(pData, p.size(), (pDatai+1)*p.size());
+                }
+                SubList<scalar>(pData, p.size(), pDatai*p.size()) =
+                    p.internalField();
+            }
+        }
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
