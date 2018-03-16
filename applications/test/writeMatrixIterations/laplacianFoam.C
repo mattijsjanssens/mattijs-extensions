9c90c77c77b6e971a8f0fb7c7a61c0bb1f180f10
Mattijs Janssens <mattijs.janssens@gmail.com>
2018-03-16 17:29:33 +0000

    ENH: PCG: added solver result dumping

--- applications/test/writeMatrixIterations/laplacianFoam.C
+++ applications/test/writeMatrixIterations/laplacianFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 
 DebugVar(solDict);
 
-    while (simple.loop())
+    while (simple.loop(runTime))
     {
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
