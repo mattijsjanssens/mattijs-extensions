2985b90216533a19a8f9666e302f1eedaf50b03d
mattijs <mattijs>
2019-12-17 10:05:25 +0000

    ENH: PPCG: pipelined CR

---
+++ applications/test/preconditioned_cg/PPCG.C
@@ -0,0 +1,198 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "PPCG.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(PPCG, 0);
+
+    lduMatrix::solver::addsymMatrixConstructorToTable<PPCG>
+        addPPCGSymMatrixConstructorToTable_;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::PPCG::PPCG
+(
+    const word& fieldName,
+    const lduMatrix& matrix,
+    const FieldField<Field, scalar>& interfaceBouCoeffs,
+    const FieldField<Field, scalar>& interfaceIntCoeffs,
+    const lduInterfaceFieldPtrsList& interfaces,
+    const dictionary& solverControls
+)
+:
+    lduMatrix::solver
+    (
+        fieldName,
+        matrix,
+        interfaceBouCoeffs,
+        interfaceIntCoeffs,
+        interfaces,
+        solverControls
+    )
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::solverPerformance Foam::PPCG::solve
+(
+    scalarField& psi,
+    const scalarField& source,
+    const direction cmpt
+) const
+{
+    // --- Setup class containing solver performance data
+    solverPerformance solverPerf
+    (
+        lduMatrix::preconditioner::getName(controlDict_) + typeName,
+        fieldName_
+    );
+
+    label nCells = psi.size();
+
+    scalar* __restrict__ psiPtr = psi.begin();
+
+    scalarField pA(nCells);
+    scalar* __restrict__ pAPtr = pA.begin();
+
+    scalarField wA(nCells);
+    scalar* __restrict__ wAPtr = wA.begin();
+
+    scalar wArA = solverPerf.great_;
+    scalar wArAold = wArA;
+
+    // --- Calculate A.psi
+    matrix_.Amul(wA, psi, interfaceBouCoeffs_, interfaces_, cmpt);
+
+    // --- Calculate initial residual field
+    scalarField rA(source - wA);
+    scalar* __restrict__ rAPtr = rA.begin();
+
+    // --- Calculate normalisation factor
+    scalar normFactor = this->normFactor(psi, source, wA, pA);
+
+    if (lduMatrix::debug >= 2)
+    {
+        Info<< "   Normalisation factor = " << normFactor << endl;
+    }
+
+    // --- Calculate normalised residual norm
+    solverPerf.initialResidual() =
+        gSumMag(rA, matrix().mesh().comm())
+       /normFactor;
+    solverPerf.finalResidual() = solverPerf.initialResidual();
+
+    // --- Check convergence, solve if not converged
+    if
+    (
+        minIter_ > 0
+     || !solverPerf.checkConvergence(tolerance_, relTol_)
+    )
+    {
+        // --- Select and construct the preconditioner
+        autoPtr<lduMatrix::preconditioner> preconPtr =
+        lduMatrix::preconditioner::New
+        (
+            *this,
+            controlDict_
+        );
+
+
+        // --- Precondition residual (= u0)
+        scalarField u(nCells);
+        preconPtr->precondition(u, rA, cmpt);
+
+        // --- Calculate A*u
+        scalarField w(nCells);
+        matrix_.Amul(w, u, interfaceBouCoeffs_, interfaces_, cmpt);
+
+
+        scalarField m(nCells);
+
+        // --- Solver iteration
+        do
+        {
+            // --- Store previous wArA
+            wArAold = wArA;
+
+            // --- Precondition residual
+            preconPtr->precondition(m, w, cmpt);
+
+            // --- Update search directions:
+            const scalar gamma = gSumProd(w, u, matrix().mesh().comm());
+            const scalar delta = gSumProd(m, w, matrix().mesh().comm());
+
+            matrix_.Amul(n, m, interfaceBouCoeffs_, interfaces_, cmpt);
+
+            scalar alpha;
+            if (solverPerf.nIterations() == 0)
+            {
+                alpha = gamma/delta;
+                z = n;
+                q = m;
+                p = u;
+            }
+            else
+            {
+                const scalar beta = gamma/gammaold;
+                alpha = gamma/(delta-beta*gamma/alphaold);
+
+                z = n + beta*z;
+                q = m + beta*q;
+                p = u + beta*p;
+            }
+
+            for (label cell=0; cell<nCells; cell++)
+            {
+                psiPtr[cell] += alpha*pAPtr[cell];
+                u[cell] -= alpha*q[cell];
+                w[cell] -= alpha*z[cell];
+            }
+
+            solverPerf.finalResidual() =
+                gSumMag(rA, matrix().mesh().comm())
+               /normFactor;
+
+        } while
+        (
+            (
+              ++solverPerf.nIterations() < maxIter_
+            && !solverPerf.checkConvergence(tolerance_, relTol_)
+            )
+         || solverPerf.nIterations() < minIter_
+        );
+    }
+
+    return solverPerf;
+}
+
+
+// ************************************************************************* //
