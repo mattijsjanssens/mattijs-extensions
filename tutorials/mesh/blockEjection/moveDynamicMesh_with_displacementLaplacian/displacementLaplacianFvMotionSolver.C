1b3712c804d9ae0161eb46c3730a2209d6dbd31c
mattijs <mattijs>
2020-09-11 16:44:17 +0100

    ENH: volPointInterpolation: handle cyclicAMI

---
+++ tutorials/mesh/blockEjection/moveDynamicMesh_with_displacementLaplacian/displacementLaplacianFvMotionSolver.C
@@ -0,0 +1,466 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2015-2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "displacementLaplacianFvMotionSolver.H"
+#include "motionInterpolation.H"
+#include "motionDiffusivity.H"
+#include "fvmLaplacian.H"
+#include "addToRunTimeSelectionTable.H"
+#include "OFstream.H"
+#include "meshTools.H"
+#include "mapPolyMesh.H"
+#include "fvOptions.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(displacementLaplacianFvMotionSolver, 0);
+
+    addToRunTimeSelectionTable
+    (
+        motionSolver,
+        displacementLaplacianFvMotionSolver,
+        dictionary
+    );
+
+    addToRunTimeSelectionTable
+    (
+        displacementMotionSolver,
+        displacementLaplacianFvMotionSolver,
+        displacement
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::displacementLaplacianFvMotionSolver::displacementLaplacianFvMotionSolver
+(
+    const polyMesh& mesh,
+    const IOdictionary& dict
+)
+:
+    displacementMotionSolver(mesh, dict, typeName),
+    fvMotionSolver(mesh),
+    cellDisplacement_
+    (
+        IOobject
+        (
+            "cellDisplacement",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        fvMesh_,
+        dimensionedVector(pointDisplacement_.dimensions(), Zero),
+        cellMotionBoundaryTypes<vector>(pointDisplacement_.boundaryField())
+    ),
+    pointLocation_(nullptr),
+    interpolationPtr_
+    (
+        coeffDict().found("interpolation")
+      ? motionInterpolation::New(fvMesh_, coeffDict().lookup("interpolation"))
+      : motionInterpolation::New(fvMesh_)
+    ),
+    diffusivityPtr_
+    (
+        motionDiffusivity::New(fvMesh_, coeffDict().lookup("diffusivity"))
+    ),
+    frozenPointsZone_
+    (
+        coeffDict().found("frozenPointsZone")
+      ? fvMesh_.pointZones().findZoneID
+        (
+            coeffDict().get<word>("frozenPointsZone")
+        )
+      : -1
+    )
+{
+    IOobject io
+    (
+        "pointLocation",
+        fvMesh_.time().timeName(),
+        fvMesh_,
+        IOobject::MUST_READ,
+        IOobject::AUTO_WRITE
+    );
+
+    if (debug)
+    {
+        Info<< "displacementLaplacianFvMotionSolver:" << nl
+            << "    diffusivity       : " << diffusivityPtr_().type() << nl
+            << "    frozenPoints zone : " << frozenPointsZone_ << endl;
+    }
+
+
+    if (io.typeHeaderOk<pointVectorField>(true))
+    {
+        pointLocation_.reset
+        (
+            new pointVectorField
+            (
+                io,
+                pointMesh::New(fvMesh_)
+            )
+        );
+
+        if (debug)
+        {
+            Info<< "displacementLaplacianFvMotionSolver :"
+                << " Read pointVectorField "
+                << io.name()
+                << " to be used for boundary conditions on points."
+                << nl
+                << "Boundary conditions:"
+                << pointLocation_().boundaryField().types() << endl;
+        }
+    }
+}
+
+
+Foam::displacementLaplacianFvMotionSolver::
+displacementLaplacianFvMotionSolver
+(
+    const polyMesh& mesh,
+    const IOdictionary& dict,
+    const pointVectorField& pointDisplacement,
+    const pointIOField& points0
+)
+:
+    displacementMotionSolver(mesh, dict, pointDisplacement, points0, typeName),
+    fvMotionSolver(mesh),
+    cellDisplacement_
+    (
+        IOobject
+        (
+            "cellDisplacement",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        fvMesh_,
+        dimensionedVector(pointDisplacement_.dimensions(), Zero),
+        cellMotionBoundaryTypes<vector>(pointDisplacement_.boundaryField())
+    ),
+    pointLocation_(nullptr),
+    interpolationPtr_
+    (
+        coeffDict().found("interpolation")
+      ? motionInterpolation::New(fvMesh_, coeffDict().lookup("interpolation"))
+      : motionInterpolation::New(fvMesh_)
+    ),
+    diffusivityPtr_
+    (
+        motionDiffusivity::New(fvMesh_, coeffDict().lookup("diffusivity"))
+    ),
+    frozenPointsZone_
+    (
+        coeffDict().found("frozenPointsZone")
+      ? fvMesh_.pointZones().findZoneID
+        (
+            coeffDict().get<word>("frozenPointsZone")
+        )
+      : -1
+    )
+{
+    IOobject io
+    (
+        "pointLocation",
+        fvMesh_.time().timeName(),
+        fvMesh_,
+        IOobject::MUST_READ,
+        IOobject::AUTO_WRITE
+    );
+
+    if (debug)
+    {
+        Info<< "displacementLaplacianFvMotionSolver:" << nl
+            << "    diffusivity       : " << diffusivityPtr_().type() << nl
+            << "    frozenPoints zone : " << frozenPointsZone_ << endl;
+    }
+
+
+    if (io.typeHeaderOk<pointVectorField>(true))
+    {
+        pointLocation_.reset
+        (
+            new pointVectorField
+            (
+                io,
+                pointMesh::New(fvMesh_)
+            )
+        );
+
+        if (debug)
+        {
+            Info<< "displacementLaplacianFvMotionSolver :"
+                << " Read pointVectorField "
+                << io.name()
+                << " to be used for boundary conditions on points."
+                << nl
+                << "Boundary conditions:"
+                << pointLocation_().boundaryField().types() << endl;
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::displacementLaplacianFvMotionSolver::
+~displacementLaplacianFvMotionSolver()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::motionDiffusivity&
+Foam::displacementLaplacianFvMotionSolver::diffusivity()
+{
+    if (!diffusivityPtr_)
+    {
+        diffusivityPtr_ = motionDiffusivity::New
+        (
+            fvMesh_,
+            coeffDict().lookup("diffusivity")
+        );
+    }
+
+    return *diffusivityPtr_;
+}
+
+
+Foam::tmp<Foam::pointField>
+Foam::displacementLaplacianFvMotionSolver::curPoints() const
+{
+    interpolationPtr_->interpolate
+    (
+        cellDisplacement_,
+        pointDisplacement_
+    );
+
+    // Explicitly normalise if any override of constraint patches. The idea
+    // is that the sum used to normalise the weights uses the default
+    // patch types but sometimes the bcs on 'cellDisplacement' get overridden
+    // (e.g. to get sliding on cyclicACMI and not interpolate 'through' the 
+    //  AMI)
+    {
+        // Check if any override. We are conservative and assume if any override
+        // that means the interpolation has been overridden
+        const pointVectorField::Boundary& bfld =
+            pointDisplacement_.boundaryField();
+
+        bool patchTypeOverride = false;
+        for (const auto& pfld : bfld)
+        {
+            if (pfld.type() != pfld.patch().constraintType())
+            {
+                patchTypeOverride = true;
+                break;
+            }
+        }
+        reduce(patchTypeOverride, orOp<bool>());
+
+        if (patchTypeOverride)
+        {
+            // Re-normalise the weights. Done by seeing what a 'one' field
+            // (with same bcs as pointDisplacement) would get interpolated as.
+
+            const fvMesh& mesh = cellDisplacement_.mesh();
+
+            const volScalarField cellOne
+            (
+                IOobject
+                (
+                    "cellOne",
+                    mesh.polyMesh::instance(),
+                    mesh,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE,
+                    false
+                ),
+                mesh,
+                dimensionedScalar(dimless, scalar(1.0))
+            );
+
+            const pointMesh& pMesh = pointDisplacement_.mesh();
+
+            // Make 'calculated' on all non-constraint patches.
+            wordList wantedBCs(bfld.types());
+            wordList patchTypes(bfld.size());
+            forAll(bfld, patchi)
+            {
+                patchTypes[patchi] = bfld[patchi].patch().constraintType();
+                if
+                (
+                    (patchTypes[patchi] == word::null)
+                 || (patchTypes[patchi] != bfld[patchi].type())
+                )
+                {
+                    wantedBCs[patchi] =
+                        pointPatchField<scalar>::calculatedType();
+                }
+            }   
+
+            // Generate field with same constraint patch override
+            pointScalarField pointOne
+            (
+                IOobject
+                (
+                    "pointOne",
+                    mesh.polyMesh::instance(),
+                    pMesh.thisDb(),
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE,
+                    false
+                ),
+                pMesh,
+                dimensionedScalar(dimless, Zero),
+                wantedBCs,
+                patchTypes
+            );
+
+            // Do interpolation. End result should be 1 on all 'normal' patches.
+            interpolationPtr_->interpolate
+            (
+                cellOne,
+                pointOne
+            );
+
+            pointDisplacement_ /= pointOne;
+        }
+    }
+
+
+
+    if (pointLocation_)
+    {
+        if (debug)
+        {
+            Info<< "displacementLaplacianFvMotionSolver : applying "
+                << " boundary conditions on " << pointLocation_().name()
+                << " to new point location."
+                << endl;
+        }
+
+        pointLocation_().primitiveFieldRef() =
+            points0()
+          + pointDisplacement_.primitiveField();
+
+        pointLocation_().correctBoundaryConditions();
+
+        // Implement frozen points
+        if (frozenPointsZone_ != -1)
+        {
+            const pointZone& pz = fvMesh_.pointZones()[frozenPointsZone_];
+
+            forAll(pz, i)
+            {
+                pointLocation_()[pz[i]] = points0()[pz[i]];
+            }
+        }
+
+        twoDCorrectPoints(pointLocation_().primitiveFieldRef());
+
+        return tmp<pointField>(pointLocation_().primitiveField());
+    }
+    else
+    {
+        tmp<pointField> tcurPoints
+        (
+            points0() + pointDisplacement_.primitiveField()
+        );
+        pointField& curPoints = tcurPoints.ref();
+
+        // Implement frozen points
+        if (frozenPointsZone_ != -1)
+        {
+            const pointZone& pz = fvMesh_.pointZones()[frozenPointsZone_];
+
+            forAll(pz, i)
+            {
+                curPoints[pz[i]] = points0()[pz[i]];
+            }
+        }
+
+        twoDCorrectPoints(curPoints);
+
+        return tcurPoints;
+    }
+}
+
+
+void Foam::displacementLaplacianFvMotionSolver::solve()
+{
+    // The points have moved so before interpolation update
+    // the motionSolver accordingly
+    movePoints(fvMesh_.points());
+
+    diffusivity().correct();
+    pointDisplacement_.boundaryFieldRef().updateCoeffs();
+
+    fv::options& fvOptions(fv::options::New(fvMesh_));
+
+    // We explicitly do NOT want to interpolate the motion inbetween
+    // different regions so bypass all the matrix manipulation.
+    fvVectorMatrix TEqn
+    (
+        fvm::laplacian
+        (
+            dimensionedScalar("viscosity", dimViscosity, 1.0)
+           *diffusivity().operator()(),
+            cellDisplacement_,
+            "laplacian(diffusivity,cellDisplacement)"
+        )
+     ==
+        fvOptions(cellDisplacement_)
+    );
+
+    fvOptions.constrain(TEqn);
+    TEqn.solveSegregatedOrCoupled(TEqn.solverDict());
+    fvOptions.correct(cellDisplacement_);
+}
+
+
+void Foam::displacementLaplacianFvMotionSolver::updateMesh
+(
+    const mapPolyMesh& mpm
+)
+{
+    displacementMotionSolver::updateMesh(mpm);
+
+    // Update diffusivity. Note two stage to make sure old one is de-registered
+    // before creating/registering new one.
+    diffusivityPtr_.clear();
+}
+
+
+// ************************************************************************* //
