7baef7bcd57ca0ea202295de842554be0923d05b
mattijs <mattijs>
2022-09-20 20:17:26 +0100

    ENH: averageConstraint: initial lsq

--- applications/solvers/incompressible/simpleFoam_with_fvOption/code/v2206/averageConstraint/averageConstraint.C
+++ applications/solvers/incompressible/simpleFoam_with_fvOption/code/v2206/averageConstraint/averageConstraint.C
@@ -29,6 +29,7 @@ License
 #include "fvMesh.H"
 #include "fvMatrices.H"
 #include "addToRunTimeSelectionTable.H"
+#include "SVD.H"
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
@@ -140,7 +141,7 @@ void Foam::fv::averageConstraint::print
 }
 
 
-void Foam::fv::averageConstraint::constrain
+void Foam::fv::averageConstraint::average
 (
     fvMatrix<scalar>& eqn,
     const label
@@ -255,6 +256,281 @@ void Foam::fv::averageConstraint::constrain
             }
         }
     }
+}
+
+
+void Foam::fv::averageConstraint::stencilWeights
+(
+    const point& sample,
+    const pointList& donorCcs,
+    scalarList& weights
+) const
+{
+    Pout<< "For sample:" << sample
+        << " have ccs:" << donorCcs
+        << endl;
+
+    // Implicit least squares weighting
+    // Number of donors
+    label nD = donorCcs.size();
+
+    weights.setSize(nD);
+
+    // List for distance vectors and LSQ weights
+    List<vector> d(nD);
+    scalarList LSQw(nD);
+
+    // Sum of weights
+    scalar W = 0;
+
+    // Sum of weighted distance vectors
+    vector dw(Zero);
+
+    RectangularMatrix<scalar> A(nD, 3);
+
+    bool shortC = false;
+
+    // Compute distance vectors and fill rectangular matrix
+    forAll(donorCcs, j)
+    {
+        // Neighbour weights
+        d[j] = donorCcs[j] - sample;
+
+        // Check for short-circuiting if zero distance
+        // is detected with respect to any donor
+        if (mag(d[j]) < ROOTVSMALL)
+        {
+            shortC = true;
+            break;
+        }
+
+        LSQw[j] = 1.0/magSqr(d[j]);
+
+        // T matrix
+        vector wd = LSQw[j]*d[j];
+        A[j][0] = wd.x();
+        A[j][1] = wd.y();
+        A[j][2] = wd.z();
+
+        // Sum of weighted distance vectors
+        dw += wd;
+
+        // Sum of weights
+        W += LSQw[j];
+    }
+
+    if (!shortC)
+    {
+        // Use Singular Value Decomposition to avoid problems
+        // with 1D, 2D stencils
+        SVD svd(A.T()*A, SMALL);
+
+        // Least squares vectors
+        RectangularMatrix<scalar> ATAinvAT(svd.VSinvUt()*A.T());
+
+        scalar saveDiag(W);
+
+        // Diagonal coefficient
+        for (label i = 0; i < 3; i++)
+        {
+            // Get row
+            scalarList Row(UList<scalar>(ATAinvAT[i], nD));
+
+            forAll(donorCcs, j)
+            {
+                W -= Row[j]*LSQw[j]*dw[i];
+            }
+        }
+
+        if (mag(W) < SMALL*mag(saveDiag))
+        {
+            shortC = true;
+        }
+        else
+        {
+            // Compute final neighbour weights with  additional scaling
+            forAll(donorCcs, j)
+            {
+                weights[j] =
+                (
+                    LSQw[j]
+                - ATAinvAT[0][j]*LSQw[j]*dw[0]
+                - ATAinvAT[1][j]*LSQw[j]*dw[1]
+                - ATAinvAT[2][j]*LSQw[j]*dw[2]
+                )
+            /W;
+            }
+        }
+    }
+
+    if (shortC)
+    {
+        // Matrix ill conditioned. Use straight injection from central
+        // donor.
+        weights = 0.0;
+        weights[0] = 1.0;
+    }
+}
+void Foam::fv::averageConstraint::interpolate
+(
+    fvMatrix<scalar>& eqn,
+    const label
+)
+{
+    Pout
+        << "averageConstraint::constrain"
+        << " for field " << eqn.psi().name() << endl;
+
+    if (!eqn.symmetric() && !eqn.asymmetric())
+    {
+        return;
+    }
+
+    const cellList& cells = mesh_.cells();
+    const labelUList& own = mesh_.owner();
+    const labelUList& nei = mesh_.neighbour();
+    const auto& C = mesh_.C();
+
+
+    // Collect neighbour cell centres
+
+    DynamicList<point> donorCcs;
+    DynamicList<label> faceIDs;
+    DynamicList<label> patchIDs;
+    for (const label celli : cells_)
+    {
+        Pout<< "OLD: For cell:" << celli
+            << " at:" << mesh_.cellCentres()[celli]
+            << " diag:" << eqn.diag()[celli]
+            << " source:" << eqn.source()[celli]
+            << endl;
+
+        // Count number of valid contributions (most boundary faces will not
+        // add a contribution - only coupled ones will)
+        donorCcs.clear();
+        faceIDs.clear();
+        patchIDs.clear();
+        for (const label facei : cells[celli])
+        {
+            if (mesh_.isInternalFace(facei))
+            {
+                const label nbrCelli
+                (
+                    (celli == own[facei])
+                  ? nei[facei]
+                  : own[facei]
+                );
+                donorCcs.append(C[nbrCelli]);
+                faceIDs.append(facei);
+                patchIDs.append(-1);
+            }
+            else
+            {
+                const label patchi = mesh_.boundaryMesh().whichPatch(facei);
+                const auto& pp = mesh_.boundaryMesh()[patchi];
+
+                if (eqn.boundaryCoeffs()[patchi].size())
+                {
+                    const label patchFacei = pp.whichFace(facei);
+                    donorCcs.append(C.boundaryField()[patchi][patchFacei]);
+                    faceIDs.append(facei);
+                    patchIDs.append(patchi);
+                }
+            }
+        }
+        scalarList weights;
+        stencilWeights
+        (
+            C[celli],
+            donorCcs,
+            weights
+        );
+
+        const scalar sumWeights = sum(weights);
+
+//XXXXXX
+        // Insert as averaging stencil
+        const scalar oldDiag = eqn.diag()[celli];
+
+        eqn.source()[celli] = 0.0;
+        eqn.diag()[celli]*= sumWeights;
+
+        // And set psi already to average
+        eqn.psi()[celli] = Zero;
+
+        // Set all off diagonal contributions
+        forAll(faceIDs, i)
+        {
+            const scalar w = oldDiag*weights[i];
+            const label facei = faceIDs[i];
+
+            if (mesh_.isInternalFace(facei))
+            {
+                const label nbrCelli
+                (
+                    (celli == own[facei])
+                  ? nei[facei]
+                  : own[facei]
+                );
+
+                if (eqn.symmetric())
+                {
+                    // Only upper is set
+                    eqn.upper()[facei] = w;
+                }
+                else
+                {
+                    if (celli == own[facei])
+                    {
+                        eqn.upper()[facei] = w;
+                    }
+                    else
+                    {
+                        eqn.lower()[facei] = w;
+                    }
+                }
+
+                eqn.psi()[celli] += w*eqn.psi()[nbrCelli];
+            }
+            else
+            {
+                const label patchi = mesh_.boundaryMesh().whichPatch(facei);
+                const auto& pp = mesh_.boundaryMesh()[patchi];
+
+                if (eqn.boundaryCoeffs()[patchi].size())
+                {
+                    const label patchFacei = pp.whichFace(facei);
+                    eqn.boundaryCoeffs()[patchi][patchFacei] = w;
+                    eqn.psi()[celli] +=
+                        w*eqn.psi().boundaryField()[patchi][patchFacei];
+                }
+            }
+        }
+        eqn.psi()[celli] /= sumWeights;
+
+        {
+            print(eqn, celli);
+            const labelList& cCells = mesh_.cellCells()[celli];
+            for (const label celli : cCells)
+            {
+                Pout<< "    Neighbour:" << celli
+                    << " at:" << mesh_.cellCentres()[celli]
+                    << " value:" << eqn.psi()[celli]
+                    << endl;
+            }
+        }
+    }
+}
+
+
+void Foam::fv::averageConstraint::constrain
+(
+    fvMatrix<scalar>& eqn,
+    const label fieldi
+)
+{
+    //average(eqn, field);
+    interpolate(eqn, fieldi);
 }
 
 
